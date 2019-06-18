474222f75ba1b4acc744a9f4927bd10a2ac63d87
SkyTradeInc <38687801+SkyTradeInc@users.noreply.github.com>
2019-06-19 02:47:57 +1000

    add this.syncing check to not resort back to manual syncing

--- components/BlockchainSync.js
+++ components/BlockchainSync.js
@@ -235,7 +235,7 @@ class BlockchainSync {
       this.lastBlockProcessed = data[0] && data[0].number ? data[0].number : 0
       const lastBlockProcessed = data[0] && data[0].number ? data[0].number : 0
       this.latestBlock = data[1].number
-      if(lastBlockProcessed < data[1].number) {
+      if(this.syncing && lastBlockProcessed < data[1].number) {
         const estimatedTimeLeft = ((data[1]-lastBlockProcessed)*this.average)
         console.log(chalk.yellow(`[!] Status: NOT SYNC `)) //`${chalk.cyan(lastBlockProcessed.toLocaleString())}/${chalk.cyan(data[1].number.toLocaleString())} blocks (${((lastBlockProcessed/data[1].number)*100).toFixed(2)}%)`))
         console.log(chalk.yellow(`[!] Average Speed: ${process.env.SYNC_REQUESTS} per ${(this.average/1000).toFixed(2)} second(s)`))
