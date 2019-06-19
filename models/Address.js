d4402fe1d0d2d4d95db1a51ac55be28bb123d95f
SkyTradeInc <38687801+SkyTradeInc@users.noreply.github.com>
2019-06-19 21:36:17 +1000

    add transaction array

--- models/Address.js
+++ models/Address.js
@@ -5,6 +5,7 @@ const addressSchema = new Schema({
      address: { type: String, index: { unique: true } },
      balance: Number,
      blockNumber: Number,
+     transactions: [],
      type: { type: Number, default: 0 }, // address: 0x0, contract: 0x1
 });
 
